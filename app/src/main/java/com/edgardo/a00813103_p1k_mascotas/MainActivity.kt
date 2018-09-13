26e084c50460fd62d3cc9a21d15c066ebfe914b3
Edgardo Acosta Leal <edgardo_acostaleal@hotmail.com>
2018-09-12 22:04:51 -0500

    Added option to see favorites add/delete from favorite list. Fix image Bitmap

--- app/src/main/java/com/edgardo/a00813103_p1k_mascotas/MainActivity.kt
+++ app/src/main/java/com/edgardo/a00813103_p1k_mascotas/MainActivity.kt
@@ -12,7 +12,6 @@ package com.edgardo.a00813103_p1k_mascotas
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -20,35 +19,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import com.edgardo.a00813103_p1k_mascotas.R.styleable.Snackbar
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import kotlinx.android.synthetic.main.row.*
 
-var list_favorits : ArrayList<Int> = arrayListOf()
+//var list_favorits: ArrayList<Int> = arrayListOf()
+var list_favorits: HashMap<Int, Mascota> = hashMapOf()
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
 
     companion object {
 
-        const val BUTTON_CLICKED: String = "show or edit"
+        const val BUTTON_CLICKED: String = "showOrEdit"
         const val NAME_KEY: String = "nombre"
         const val Raza_KEY: String = "raza"
         const val EMAIL_KEY: String = "email"
         const val PHONE_KEY: String = "phone"
         const val ADDRESS_KEY: String = "address"
+        const val FAVORITE_KEY: String = "favorito"
         const val ID_IMAGE_KEY: String = "idImage"
+        const val POSITION_KEY: String = "idImage"
+        const val MASCOT_LIST: String = "listamascota"
+
 
     }
 
-    var adapterMascosta: MascotaAdapter? = null
-    var mascotList: ArrayList<Mascota>? = arrayListOf() //= MacostaData().listamascotas
+    lateinit var adapterMascosta: MascotaAdapter
+    var mascotList: ArrayList<Mascota> = arrayListOf() //= MacostaData().listamascotas
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar_main.title = resources.getString(R.string.title_activity_main)
+//        setTitle(resources.getString(R.string.title_activity_main))
+
 
         fab_registrar.setOnClickListener { view ->
             val intent = Intent(this, RegisterActivity::class.java)
@@ -61,7 +66,20 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
 //                    .setAction("Action", null).show()
 
         }
-        if (mascotList!!.size == 0) {
+
+        fab_favorito.setOnClickListener { v: View? ->
+            val intent = Intent(this, FavoritesActivity::class.java)
+
+            intent.putExtra("list_favorites", list_favorits)
+            startActivityForResult(intent, 2)
+        }
+
+        if (savedInstanceState != null) {
+            mascotList = savedInstanceState.getSerializable(MASCOT_LIST) as ArrayList<Mascota>
+        }
+
+
+        if (mascotList.size == 0) {
             Toast.makeText(applicationContext,
                     "No dogs on list", Toast.LENGTH_SHORT).show()
         }
@@ -78,20 +96,33 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
 
     }
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putSerializable(MASCOT_LIST, mascotList)
+    }
+
     override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         val mascosta: Mascota = mascotList!![position]
         Log.d("Row Clicked", position.toString())
 
-        val intent = Intent(this, RegisterActivity::class.java)
+        val intentShow = Intent(this, RegisterActivity::class.java)
 
-        intent.putExtra(BUTTON_CLICKED, "show")
-        intent.putExtra(NAME_KEY, mascosta.Nombre)
-        intent.putExtra(Raza_KEY, mascosta.Raza)
-        intent.putExtra(EMAIL_KEY, mascosta.Correo)
-        intent.putExtra(PHONE_KEY, mascosta.Telefono)
-        intent.putExtra(ADDRESS_KEY, mascosta.Ubicacion)
-        intent.putExtra(ID_IMAGE_KEY, mascosta.Id_Imagen)
-        startActivity(intent)
+        intentShow.putExtra("BUTTON_CLICKED", "show")
+        intentShow.putExtra(NAME_KEY, mascosta.Nombre)
+        intentShow.putExtra(Raza_KEY, mascosta.Raza)
+        intentShow.putExtra(EMAIL_KEY, mascosta.Correo)
+        intentShow.putExtra(PHONE_KEY, mascosta.Telefono)
+        intentShow.putExtra("ADDRESS_KEY", mascosta.Ubicacion)
+        Log.d("Mascosta", mascosta.Ubicacion)
+        intentShow.putExtra(FAVORITE_KEY, mascosta.Favorito)
+        intentShow.putExtra("ID_IMAGE_KEY", mascosta.Id_Imagen)
+        intentShow.putExtra(POSITION_KEY, position)
+        intentShow.putExtra("SOURECE", "Main")
+
+
+//        startActivity(intentShow)
+        startActivityForResult(intentShow, 10)
     }
 
 
@@ -102,14 +133,27 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
         if (requestCode == 0) {
             if (resultCode == Activity.RESULT_OK) {
                 val newMascota = data?.getSerializableExtra(RegisterActivity.NEW_REGISTER) as Mascota
-                val thumbnailBitmap = data.getParcelableExtra<Bitmap>("thumbnailBitmap")
-                Log.d("Main-Resume", newMascota.Nombre)
-                Log.d("Bitmap", thumbnailBitmap.toString())
-                newMascota.Id_Imagen = thumbnailBitmap
+
                 mascotList!!.add(newMascota)
+                list_favorits.clear()
                 adapterMascosta?.notifyDataSetChanged()
             }
 
+        } else if (requestCode == 10) {
+            if (resultCode == Activity.RESULT_OK) {
+                Log.d("Modify Favorite", requestCode.toString())
+                val pos = data?.extras?.getInt("positionModify")
+                val newValue = data?.extras?.getBoolean("valueModify")
+                if (pos != null) {
+                    val auxInt: Int = pos
+
+                    mascotList[auxInt].Favorito = newValue!!
+                    list_favorits.clear()
+                    adapterMascosta?.notifyDataSetChanged()
+                }
+
+
+            }
         }
 
     }
@@ -125,23 +169,21 @@ class MascotaAdapter(context: Context, mascotas: ArrayList<Mascota>) :
         val mascota = getItem(position)
 
         val tvName = rowView.findViewById(R.id.text_nombre) as TextView
-        var tvRaza = rowView.findViewById(R.id.text_raza) as TextView
-        var tvDate = rowView.findViewById(R.id.text_fecha_agregado) as TextView
+        val tvRaza = rowView.findViewById(R.id.text_raza) as TextView
+        val tvDate = rowView.findViewById(R.id.text_fecha_agregado) as TextView
         val ivDog = rowView.findViewById(R.id.image_mascota) as ImageView
-        val favorito = rowView.findViewById(R.id.button_favorito) as ImageButton
 
 
-        var dogName = context.resources.getStringArray(R.array.razas_array)[mascota!!.Raza]
+        val dogName = context.resources.getStringArray(R.array.razas_array)[mascota!!.Raza]
 
         tvName.text = mascota!!.Nombre
         tvRaza.text = dogName
         tvDate.text = mascota!!.Fecha
 
+        if (mascota.Favorito) {
+            list_favorits.put(position, mascota)
 
-        favorito.setOnClickListener { v: View? ->
-//            click_Favorito(v!!, mascota)
-            Toast.makeText(context,
-                    "Click button", Toast.LENGTH_SHORT).show()
+
         }
 
 
@@ -149,19 +191,7 @@ class MascotaAdapter(context: Context, mascotas: ArrayList<Mascota>) :
         return rowView
     }
 
-    private fun click_Favorito(v: View, mascota: Mascota) {
 
-        Log.d("View Id", v.id.toString())
-        val favorito = v.findViewById(R.id.button_favorito) as ImageButton
-//        context.resources.drawable(favorito)
-//
-//        if (context.resources.getDrawable(favorito) == getDrawable R.drawable.ic_star_full) {
-//            mascota.Favorito = false
-//            favorito.setImageResource(R.drawable.ic_star_empty)
-//        } else {
-//            mascota.Favorito = false
-//            favorito.setImageResource(R.drawable.ic_star_full)
-//        }
-    }
 }
+
 
--- app/src/main/java/com/edgardo/a00813103_p1k_mascotas/RegisterActivity.kt
+++ app/src/main/java/com/edgardo/a00813103_p1k_mascotas/RegisterActivity.kt
@@ -21,7 +21,9 @@ import android.os.PersistableBundle
 import android.provider.MediaStore
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.widget.Spinner
 import android.widget.ArrayAdapter
@@ -45,11 +47,20 @@ class RegisterActivity : AppCompatActivity() {
 
     var hasThumbnail = false
     var thumbnailBitmap: Bitmap? = null
+    var checkBoxViewMode = false
+    var position: Int? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+        val myToolbar = findViewById<View>(R.id.toolbar_register) as Toolbar
+        myToolbar.title = ""
+        setSupportActionBar(myToolbar)
+
+        actionBar?.setDisplayHomeAsUpEnabled(true)
 
 
         // Check orientation of vie
@@ -82,23 +93,44 @@ class RegisterActivity : AppCompatActivity() {
         }
 
 
+        button_save.setOnClickListener { click(it) }
+        button_cancel.setOnClickListener { click(it) }
         // Set Listener for the camera button
         button_image.setOnClickListener { click(it) }
 
+        checkBox_favorites.setOnClickListener { checkBoxClicker(it) }
+
 
         // Read value from MainActivity
+
         val extras = intent.extras ?: return
 
 
 
-        when (extras.getString(MainActivity.BUTTON_CLICKED)) {
+        when (extras.getString("BUTTON_CLICKED")) {
             "show" -> {
+
                 val lbname = extras.getString(MainActivity.NAME_KEY)
+                toolbar_register.title = resources.getString(R.string.title_activity_register_view) + lbname
+
                 val spinerRaza = extras.getInt(MainActivity.Raza_KEY)
                 val lbemail = extras.getString(MainActivity.EMAIL_KEY)
                 val lbphone = extras.getString(MainActivity.PHONE_KEY)
-                val lbaddress = extras.getString(MainActivity.ADDRESS_KEY)
-                val idImagen = extras.get(MainActivity.ID_IMAGE_KEY) as Bitmap
+                val lbaddress = extras.getString("ADDRESS_KEY")
+                val chBoxFav = extras.getBoolean(MainActivity.FAVORITE_KEY)
+                val idImagen = extras.getParcelable("ID_IMAGE_KEY") as? Bitmap
+                val source = extras.getString("SOURECE")
+
+
+
+                position = extras.getInt(MainActivity.POSITION_KEY)
+
+
+                checkBox_favorites.isChecked = chBoxFav
+
+                if (source.equals("Favorite")) {
+                    checkBox_favorites.isEnabled = false
+                }
 
                 image_foto.setImageBitmap(idImagen)
 
@@ -108,6 +140,7 @@ class RegisterActivity : AppCompatActivity() {
                 spinner_razas.setSelection(spinerRaza)
                 spinner_razas.isEnabled = false
                 spinner_razas.isClickable = false
+
 
 
                 label_email.setText(lbemail)
@@ -123,19 +156,21 @@ class RegisterActivity : AppCompatActivity() {
                 button_image.isCursorVisible = false
                 button_save.isEnabled = false
                 button_save.isCursorVisible = false
+
                 Toast.makeText(applicationContext,
                         "${resources.getString(R.string.register_mode_show)}", Toast.LENGTH_SHORT).show()
 
 
             }
             "register" -> {
+                toolbar_register.title = resources.getString(R.string.title_activity_register_add)
                 Toast.makeText(applicationContext,
                         "${resources.getString(R.string.register_mode_register)}", Toast.LENGTH_SHORT).show()
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                    .setAction("Action", null).show()
-                button_save.setOnClickListener { click(it) }
             }
             "edit" -> {
+                toolbar_register.title = "Edit mode"
                 Toast.makeText(applicationContext,
                         "${resources.getString(R.string.register_mode_edit)}", Toast.LENGTH_SHORT).show()
 
@@ -148,36 +183,53 @@ class RegisterActivity : AppCompatActivity() {
 
     }
 
-
+    // Click Listener
     fun click(view: View) {
 
+        button_cancel
         when (view.id) {
             R.id.button_save -> {
-                if (validateInputs()) {
+
+                if (!checkBoxViewMode) {
+                    if (validateInputs()) {
+                        Toast.makeText(applicationContext,
+                                "Dog saved", Toast.LENGTH_SHORT).show()
+
+
+                        val df = SimpleDateFormat("dd-MM-yy -  h:mm a")
+                        val date = df.format(Calendar.getInstance().getTime()).toString()
+
+
+                        var newMascot = Mascota(label_name.text.toString(), spinner_razas.selectedItemPosition,
+                                label_address.text.toString(), date, label_phone.text.toString(),
+                                label_email.text.toString(), thumbnailBitmap, checkBox_favorites.isChecked)
+
+
+                        val intent = Intent(this, MainActivity::class.java)
+//                    intent.putExtra("thumbnailBitmap", thumbnailBitmap)
+
+                        intent.putExtra(NEW_REGISTER, newMascot)
+
+                        setResult(Activity.RESULT_OK, intent)
+                        finish()
+
+
+                    }
+                } else {
                     Toast.makeText(applicationContext,
-                            "Dog saved", Toast.LENGTH_SHORT).show()
-
-
-                    val df = SimpleDateFormat("dd-MM-yy -  h:mm a")
-                    val date = df.format(Calendar.getInstance().getTime()).toString()
-
-
-                    var newMascot = Mascota(label_name.text.toString(), spinner_razas.selectedItemPosition,
-                            label_address.text.toString(), date, label_phone.text.toString(),
-                            label_email.text.toString(), thumbnailBitmap)
-
+                            "Fav Change", Toast.LENGTH_SHORT).show()
 
                     val intent = Intent(this, MainActivity::class.java)
-                    intent.putExtra("thumbnailBitmap", thumbnailBitmap)
+//                    intent.putExtra("thumbnailBitmap", thumbnailBitmap)
 
-                    intent.putExtra(NEW_REGISTER, newMascot)
+                    intent.putExtra("positionModify", position)
+                    intent.putExtra("valueModify", checkBox_favorites.isChecked)
 
                     setResult(Activity.RESULT_OK, intent)
-
                     finish()
 
-
                 }
+
 
             }
             R.id.button_image -> {
@@ -186,65 +238,34 @@ class RegisterActivity : AppCompatActivity() {
                 activateCamera()
 
             }
+            R.id.button_cancel -> {
+                setResult(RESULT_CANCELED)
+                finish()
+            }
         }
     }
 
+    //
+    fun checkBoxClicker(view: View) {
 
-    private fun validateInputs(): Boolean {
+        if (!button_save.isEnabled) {
 
-        var verified = true
-
-
-        label_name.error = null
-        label_email.error = null
-        label_phone.error = null
-        label_address.error = null
-
-        if (label_name.text.toString().trim().isEmpty()) {
-            label_name.error = resources.getString(R.string.register_msg_error)
-            verified = false
-        } else if (label_email.text.toString().trim().isEmpty()) {
-            label_name.error = resources.getString(R.string.register_msg_error)
-            verified = false
-        } else if (label_phone.text.toString().trim().isEmpty()) {
-            label_name.error = resources.getString(R.string.register_msg_error)
-            verified = false
-        } else if (label_address.text.toString().trim().isEmpty()) {
-            label_name.error = resources.getString(R.string.register_msg_error)
-            verified = false
-        } else if (label_address.text.toString().trim().isEmpty()) {
-            label_name.error = resources.getString(R.string.register_msg_error)
-            verified = false
-        } else if (!hasThumbnail) {
-            button_image.error = resources.getString(R.string.register_msg_error)
-            return false
+            button_save.isEnabled = true
+            checkBoxViewMode = true
         }
 
-        return verified
-
     }
 
+    // Override functions
 
-    private fun activateCamera() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
 
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivityForResult(intent, TAKE_PHOTO_REQUEST)
+        when (item?.itemId) {
+
+            android.R.id.home -> finish()
         }
-    }
 
-
-    private fun disableEditText(editText: EditText) {
-        editText.isFocusable = false
-        editText.isEnabled = false
-        editText.isCursorVisible = false
-        editText.keyListener = null
-//        editText.setBackgroundColor(Color.TRANSPARENT)
-    }
-
-
-    fun finishActivity(v: View) {
-        finish()
+        return true
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -283,6 +304,63 @@ class RegisterActivity : AppCompatActivity() {
             outState?.putParcelable(IMAGE_THUMBNAIL_KEY, thumbnailBitmap)
         }
 
+    }
+
+    // Helpers
+
+    fun finishActivity(v: View) {
+        finish()
+    }
+
+    private fun disableEditText(editText: EditText) {
+        editText.isFocusable = false
+        editText.isEnabled = false
+        editText.isCursorVisible = false
+        editText.keyListener = null
+//        editText.setBackgroundColor(Color.TRANSPARENT)
+    }
+
+    private fun validateInputs(): Boolean {
+
+        var verified = true
+
+
+        label_name.error = null
+        label_email.error = null
+        label_phone.error = null
+        label_address.error = null
+
+        if (label_name.text.toString().trim().isEmpty()) {
+            label_name.error = resources.getString(R.string.register_msg_error)
+            verified = false
+        } else if (!hasThumbnail) {
+            button_image.error = resources.getString(R.string.register_msg_error)
+            return false
+        } else if (label_email.text.toString().trim().isEmpty()) {
+            label_email.error = resources.getString(R.string.register_msg_error)
+            verified = false
+        } else if (label_phone.text.toString().trim().isEmpty()) {
+            label_phone.error = resources.getString(R.string.register_msg_error)
+            verified = false
+        } else if (label_address.text.toString().trim().isEmpty()) {
+            label_address.error = resources.getString(R.string.register_msg_error)
+            verified = false
+        }
+
+        return verified
+
+    }
+
+    private fun activateCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, TAKE_PHOTO_REQUEST)
+        }
+    }
+
+    private fun initOnFocus(v: View) {
+        label_name.setOnFocusChangeListener { v, hasFocus -> }
     }
 
 }
